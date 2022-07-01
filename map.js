0bc215ac881b200f72aeabd649d1c22db995f0f0
rishabh <rishabhsati6505@gmail.com>
2022-07-01 21:35:27 +0530

    file updated

---
+++ map.js
@@ -0,0 +1,17 @@
+// const a=10;
+// const my = new Map();
+// my.set(10,"rishabh");
+// my.set(20,"saurabh");
+// my.set(30,"arjun");
+// console.log(`your name is ${my.get(a)}`);
+
+// var arr = new Array();
+// var arr = [1,2,3,4,5];
+// arr.reverse();
+// for(var i=0;i<5;i++){
+//     console.log(arr[i]);
+// }
+
+const name =["rishabh","sati"]
+const [first , last] = name;
+console.log(first);
